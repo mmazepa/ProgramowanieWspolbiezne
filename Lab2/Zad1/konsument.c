a4355d8fe3a22579e8f046b2b1a3fb99c8e82638
mmazepa <maksymilianmariusz@gmail.com>
2018-03-08 00:31:21 +0100

    Prac nad zadaniami z laboratorium 2 dalszy ciąg.

--- Lab2/Zad1/konsument.c
+++ Lab2/Zad1/konsument.c
@@ -2,9 +2,44 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void header()
+{
+  puts("  ┬┌─┌─┐┌┐┌┌─┐┬ ┬┌┬┐┌─┐┌┐┌┌┬┐");
+  puts("  ├┴┐│ ││││└─┐│ ││││├┤ │││ │ ");
+  puts("  ┴ ┴└─┘┘└┘└─┘└─┘┴ ┴└─┘┘└┘ ┴ ");
+}
+
+void makeLine()
+{
+  puts("  ------------------------------------------");
+}
+
+void introduction()
+{
+  header();
+  makeLine();
+  puts("  Tekst będzie odbierany linijka po linijce,");
+  puts("  w momencie, w którym producent zatwierdzi");
+  puts("  linijkę naciśnięciem klawisze ENTER.");
+}
+
+void beginning()
+{
+  system("clear");
+	puts("");
+  introduction();
+	puts("");
+}
+
 int main (void)
 {
-	puts ("Hello World!");
+  beginning();
 
+  puts("  ODBIERANY TEKST:");
+	puts("  ...");
+  //...
+
+  makeLine();
+	puts("");
 	return 0;
 }
--- Lab2/Zad1/producent.c
+++ Lab2/Zad1/producent.c
@@ -2,19 +2,47 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void header()
+{
+  puts("  ┌─┐┬─┐┌─┐┌┬┐┬ ┬┌─┐┌─┐┌┐┌┌┬┐");
+  puts("  ├─┘├┬┘│ │ │││ ││  ├┤ │││ │ ");
+  puts("  ┴  ┴└─└─┘─┴┘└─┘└─┘└─┘┘└┘ ┴ ");
+}
+
+void makeLine()
+{
+  puts("  ------------------------------------------");
+}
+
+void introduction()
+{
+  header();
+  makeLine();
+  puts("  Wpisz tekst, linijki zatwierdzaj ENTER'em,");
+	puts("  zakończ przekaz klawiszem ESC.");
+}
+
+void beginning()
+{
+  system("clear");
+	puts("");
+  introduction();
+	puts("");
+}
+
 int main (void)
 {
-	puts("");
-	puts("  PRODUCENT");
-	puts("  ------------------------------------------");
-	puts("  Wpisz tekst, linijki zatwierdzaj ENTER'em,");
-	puts("  zakończ przekaz klawiszem ESC.");
-	puts("");
+  beginning();
 
-	puts("  Twój tekst: ");
-	print("  ");
-	// read()...
+	puts("  TWÓJ TEKST: ");
+	printf("  ");
 
+  char string;
+  scanf("%s" , &string);
+
+  printf("\n  WPISANO:\n  %s\n", &string);
+
+  makeLine();
 	puts("");
 	return 0;
 }
