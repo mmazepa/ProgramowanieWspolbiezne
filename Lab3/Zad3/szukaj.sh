a86a1a3dcd15d1407ea81349fdfbac1e4ac3ec76
mmazepa <maksymilianmariusz@gmail.com>
2018-03-14 01:01:43 +0100

    Konstruowanie rozwiązania do zadania z Lab3.

--- Lab3/Zad3/szukaj.sh
+++ Lab3/Zad3/szukaj.sh
@@ -1,46 +1,60 @@
 #!/bin/bash
+clear
+echo ""
+echo "  ┌─┐┌─┐┬ ┬┬┌─┌─┐ ┬ ┌─┐┬ ┬"
+echo "  └─┐┌─┘│ │├┴┐├─┤ │ └─┐├─┤"
+echo "  └─┘└─┘└─┘┴ ┴┴ ┴└┘o└─┘┴ ┴"
+echo ""
+
+echo "  Nazwa skryptu:          $0"
+echo "  Folder do przeszukania: $1"
+echo "  Plik do znalezienia:    $2"
+echo ""
 
 declare -i x
-
 dir=$(pwd)
-
-if [ -d "$1" ]; then
-  sciezka="$dir/$1"
-  cd $sciezka
-  krok=0
-  for i in $( ls ); do
+if [ -s "$1" ]; then
+  path_to_file="$dir/$1"
+  cd $path_to_file
+  step=0
+  for i in $(ls); do
     if [ "$i" == "$2" ] ; then
-      let "x += 1"
+      let x+=1
+      echo "  [$x]: $(pwd)"
     else
-      while [[ -d $i ]]; do
-        krok+=1
-        ls
-        echo $krok
-        if [[ -d $i ]]; then
+      while [[ -s $i ]]; do
+        step+=1
+        # ls
+        # echo $step
+        if [[ -s $i ]]; then
           cd $i
         fi
-        for z in $( ls ); do
+        for z in $(ls); do
           if [[ "$z" == "$2" ]]; then
-            let "x += 1"
+            let x+=1
+            echo "  [$x]: $(pwd)"
           fi
-          for i in $( ls ); do
-            sleep 0
-          done
-         if [[ $i == $2 ]]; then
-           let "x += 1"
-         fi
-      done
+          # for i in $(ls); do
+          #   sleep 0
+          # done
+          if [[ $i == $2 ]]; then
+            let x+=1
+            echo "  [$x]: $(pwd)" # brak wydruku (???)
+          fi
+        done
       done
     fi
   done
 else
-  echo "Podaj prawidłowy parametr."
+  echo "  Podaj prawidłowy parametr."
 fi
+echo ""
 
-if [ -z $3] ; then
+if [ -z $3 ] ; then
   if [ -z $x ] ; then
-    echo "Niestety niczego nie znaleziono."
+    echo "  [WYNIK]: Niestety niczego nie znaleziono."
   else
-    echo "Plik/katalog został znaleziony."
+    echo "  [WYNIK]: Plik/katalog został znaleziony ($x razy)."
   fi
 fi
+echo ""
