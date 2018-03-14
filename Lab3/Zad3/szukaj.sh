ee543dfa548ccb2284a93d937b0fceadba42153b
mmazepa <maksymilianmariusz@gmail.com>
2018-03-14 21:36:19 +0100

    Wykończeniówka Zad3 z Lab3.

--- Lab3/Zad3/szukaj.sh
+++ Lab3/Zad3/szukaj.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 declare -i ilosc_plikow
 ilosc_plikow=0
-CHILDREN=
+procesy_potomne=""
 
 if [ "$#" -lt 2 ]; then
-	echo "Za mało argmentów, prawidłowy format to ./szukaj.sh [katalog] [plik]."
+	echo '[BŁĄD]: Za mało argumentów! Powinny być dokładnie 2.'
+  echo '        Prawidłowy format: '$0' [katalog] [plik].'
 	exit
 else
 	if [ ! -d "$1" ]; then
-		echo "'$1' nie jest katalogiem"
+		echo "[PORAŻKA]: Katalog o nazwie '$1' nie został znaleziony."
 		exit
 	fi
 fi
 
 for item in $1/*; do
 	if [ -d "$item" ]; then
-		"$0" "$item" "$2" "$$" &
-		CHILDREN+=("$!")
+		"$0" "$item" "$2" "trzeci_argument" &
+		procesy_potomne+=("$!")
 	elif [ -f "$item" ]; then
 		if [ $(basename $item) == "$2" ]; then
-			echo "[$2]: $1"
 			ilosc_plikow=$((ilosc_plikow+1))
+      echo "[SUKCES]: Plik '$2' znajduje się w $1."
 		fi
 	fi
 done
 
-for i in "${CHILDREN[@]}"; do
+for i in "${procesy_potomne[@]}"; do
 	if [ "$i" ]; then
 		wait "$i"
 		ilosc_plikow=$((ilosc_plikow+$?))
@@ -34,9 +35,9 @@ done
 
 if [ -z "$3" ]; then
 	if [ "$ilosc_plikow" -eq 0 ]; then
-		echo "Nie znaleziono żadnego pliku."
+		echo "[PORAŻKA]: Nie znaleziono żadnego pliku o nazwie '$2' w katalogu '$1'."
 	else
-		echo "Znalezionych plików: $ilosc_plikow"
+		echo "Wystąpień pliku '$2' w katalogu '$1': $ilosc_plikow"
 	fi
 fi
 
