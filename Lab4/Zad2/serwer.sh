4c27d6b7bf4daba192aa05b1c47f8a3cac152005
mmazepa <maksymilianmariusz@gmail.com>
2018-04-05 01:01:51 +0200

    Minimalne poprawki w serwer.sh

--- Lab4/Zad2/serwer.sh
+++ Lab4/Zad2/serwer.sh
@@ -6,7 +6,6 @@ function szescian {
   let potega=$1**3
 }
 
-trap "echo Caught SIGUSR1; exit 1" SIGUSR1;
 if [[ "$1" ]]; then
 	if [[ ! -p $1 ]]; then
 		echo "Łącze klienta nie odpowiada!"
@@ -15,14 +14,14 @@ if [[ "$1" ]]; then
 		echo "[serwer: $USER] Wynik: $2^3 = $potega" > $1
 	fi
 else
-	trap "rm -f $potok_serwera" EXIT
-	trap "" SIGTERM
-	trap "" SIGHUP
+  trap "echo Caught SIGUSR1; exit 1;" SIGUSR1;
+  trap "rm -f $potok_serwera" EXIT
+  trap "" SIGTERM
+  trap "" SIGHUP
 
 	if [[ ! -p $potok_serwera ]]; then
 		mkfifo $potok_serwera
 	fi
-
 	while true
 	do
 		if read odczyt < $potok_serwera; then
