a6af80a630084645a899bf706953d1c121a7f065
Mariusz Mazepa <mmazepa@sigma.inf.ug.edu.pl>
2018-04-05 13:24:47 +0200

    Stworzenie plików potrzebnych do Lab6/Zad3.

---
+++ Lab6/Zad3/klient.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+  puts ("Hello World! Client here!");
+  return 0;
+}
---
+++ Lab6/Zad3/serwer.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+  puts ("Hello World! Server here!");
+  return 0;
+}
